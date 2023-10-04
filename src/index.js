6aec5b4be257fa252dd0325e15f5329f78a65f4f
Rugwed Bodhankar <rugwed07@gmail.com>
2023-10-04 15:54:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ScrollReveal from "scrollreveal";
+import Typed from "typed.js";
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Check if ScrollReveal is defined
+// Check if ScrollReveal is defined
+if (typeof ScrollReveal !== 'not defined') {
+    // ScrollReveal configuration and initialization
+    const sr = ScrollReveal({
+        // reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    // Reveal elements with ScrollReveal
+    sr.reveal('.home-content, .heading', { origin: 'top' });
+    sr.reveal('.home-img, .portfolio-box, .contact-form', { origin: 'bottom' });
+    sr.reveal('.home-content h1,.about-img', { origin: 'left' });
+    sr.reveal('.home-content p,.about-content', { origin: 'right' });
+    
+} else {
+    // Log an error message to the console
+    console.error('ScrollReveal is not defined. Make sure you have included the ScrollReveal library.');
+
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections. forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+  
+    if(top >= offset && top < offset + height) {
+    navLinks.forEach(links => {
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+             });
+         };
+    });
+
+    //sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    //remove toggle icon and navbar when click toggle link
+    menuIcon.classList.remove('bx-x');
+    menuIcon.classList.remove('active');
+
+};
+
+const typed = new Typed('.multiple-texts', {
+    strings: ['Web Developer','Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
